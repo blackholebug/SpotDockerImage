5691b90e3c428bdf0246b73b483fc968be77841d
Jesse-vd-Linden <jesselindenn@gmail.com>
2023-08-21 17:35:13 +0200

    Gesture classification node working

--- catkin_ws/src/gesture_classification/src/gesture_node.py
+++ catkin_ws/src/gesture_classification/src/gesture_node.py
@@ -3,15 +3,33 @@
 import time
 import rospy
 from std_msgs.msg import Float32MultiArray
+import numpy as np
 
-class GestureClassification:
+from gesture_classification.gesture_classification import GestureClassification
+
+
+class GestureClassificationNode:
+    """_summary_
+    FPS = 60 frames / 15 remove six keypoint collections every classification round = 4 classification inferences as second
+    
+    """
     
     def __init__(self):
+        self.hand_keypoint_list = []
+        self.model = GestureClassification()
+        self.img_idx = 1
         pass
     
     def callback(self, data):
-        print("Received:", data.data)
-        time.sleep(1)
+        array = np.array(data.data).reshape(21, 3)
+        np.save
+        self.hand_keypoint_list.append(array)
+        
+        if len(self.hand_keypoint_list) > 60:
+            series = self.hand_keypoint_list.copy()
+            self.hand_keypoint_list = self.hand_keypoint_list[15:]
+            gesture = self.model.classify_time_series(series)
+            print(gesture)
 
     def run(self):
         rospy.init_node('listener', anonymous=True)
@@ -21,5 +39,5 @@ class GestureClassification:
 
 if __name__ == "__main__":
 
-    gc = GestureClassification()
+    gc = GestureClassificationNode()
     gc.run()
