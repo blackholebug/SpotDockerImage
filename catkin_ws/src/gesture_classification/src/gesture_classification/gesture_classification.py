25ec56336cd067c849c463e8bf8bec64b03badc8
Jesse-vd-Linden <jesselindenn@gmail.com>
2023-08-21 17:35:22 +0200

    gesture classification node working

---
+++ catkin_ws/src/gesture_classification/src/gesture_classification/gesture_classification.py
@@ -0,0 +1,11 @@
+
+
+
+class GestureClassification:
+    
+    def __init__(self):
+        pass
+    
+    def classify_time_series(self, series):
+        gesture = series ## transform to classification
+        return "no gesture"
