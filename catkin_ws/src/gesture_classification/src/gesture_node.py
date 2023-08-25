4bed624b41f76a3cf5ac184da833b3c3c7a7baba
Jesse-vd-Linden <jesselindenn@gmail.com>
2023-08-25 10:39:35 +0200

    Classification gesture node complete

--- catkin_ws/src/gesture_classification/src/gesture_node.py
+++ catkin_ws/src/gesture_classification/src/gesture_node.py
@@ -4,6 +4,7 @@ import time
 import rospy
 from std_msgs.msg import Float32MultiArray
 import numpy as np
+from joblib import load
 
 from gesture_classification.gesture_classification import GestureClassification
 
@@ -18,18 +19,29 @@ class GestureClassificationNode:
         self.hand_keypoint_list = []
         self.model = GestureClassification()
         self.img_idx = 1
-        pass
+        self.clf = load("/catkin_ws/src/gesture_classification/models/KNN_120.joblib")
+        
+    def classify_time_series(self, series):
+        predictions = []
+        for hand in series:
+            hand = np.array(hand).reshape(1, -1)
+            y = self.clf.predict(hand)
+            predictions.append(y[0])
+        
+        sign = max(set(predictions), key=predictions.count)
+        print(predictions)
+        
+        return sign
     
     def callback(self, data):
-        array = np.array(data.data).reshape(21, 3)
-        np.save
+        array = np.array(data.data)
         self.hand_keypoint_list.append(array)
         
         if len(self.hand_keypoint_list) > 60:
             series = self.hand_keypoint_list.copy()
-            self.hand_keypoint_list = self.hand_keypoint_list[15:]
-            gesture = self.model.classify_time_series(series)
-            print(gesture)
+            self.hand_keypoint_list = self.hand_keypoint_list[30:]
+            gesture = self.classify_time_series(series)
+            print(gesture) ## publisher this gesture
 
     def run(self):
         rospy.init_node('listener', anonymous=True)
